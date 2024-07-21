0d0e17655573ae77198ed4e652db785c1e3bd588
Dam Duc Hai <damduchai02@gmail.com>
2024-07-21 09:40:06 +0700

    add feature populate.js

--- populate.js
+++ populate.js
@@ -4,13 +4,13 @@ const connectDB = require("./db/connect");
 const Product = require("./models/productModel");
 const jsonProducts = require("./products.json");
 
-const port = process.env.PORT || 3000;
 const start = async (req, res) => {
   try {
     await connectDB(process.env.DATABASE_URL);
     await Product.deleteMany();
     await Product.create(jsonProducts);
     console.log("Success!!!");
+    process.exit();
   } catch (error) {
     console.log(error);
   }
